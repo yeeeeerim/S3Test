35c3ef8df08d5e5958a4cd19d290e4cce7a8e2da
yeeeeerim <gyr3904@gmail.com>
2023-05-26 01:01:31 +0900

    config: init

---
+++ src/main/java/com/example/reviews3/ReviewS3Application.java
@@ -0,0 +1,13 @@
+package com.example.reviews3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reviews3/ReviewS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviews3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
